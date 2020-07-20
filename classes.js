7edb51bc43fbcadef595bc4c03efd7c2f01f63df
BigStorm <lucasstormmartin@gmail.com>
2020-07-20 15:16:43 -0500

    progress

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
+// const Dallin = new Employee('Dallin', 'Anderson')
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +58,19 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age)
+    this.reports = [];
+  }
+  hire(newEmployee) {
+    this.reports.push(newEmployee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1)
+  }
+ }
+
 
 
 ////////// PROBLEM 3 //////////
